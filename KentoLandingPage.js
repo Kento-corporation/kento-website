78a7e9e7b5688239e3f6ddafdf4f903c95285f3f
Kento-corporation <matsuoka@kento-corporation.jp>
2025-04-23 21:06:07 +0900

    Update KentoLandingPage.js

--- KentoLandingPage.js
+++ KentoLandingPage.js
@@ -1,9 +1,1 @@
 
-export default function KentoLandingPage() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center text-center bg-green-50 p-8">
-      <h1 className="text-4xl font-bold">誠実さと品質を世界へ</h1>
-      <p className="mt-4 text-lg">KENTO – 健康の未来を支える</p>
-    </div>
-  );
-}
